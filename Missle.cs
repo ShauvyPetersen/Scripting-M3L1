0fbcadc5a42124f513bea8483da131e602896e70
ShauvyPetersen <168214351+ShauvyPetersen@users.noreply.github.com>
2024-04-27 08:37:13 +0200

    Add files via upload

---
+++ Missle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    //Fireball flight speed
+    public float speed;
+
+    void Update()
+    {
+        //The fireball clone is destroyed after 3 seconds
+        Destroy(gameObject, 3);
+
+        //Each frame the ball position is updated by the product of the forward motion vector (0,0,1),
+        //the speed of the ball and the value of the difference in seconds between the last and current frames
+
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        // The enemy is being destroyed
+        Enemy enemy = other.GetComponent<Enemy>();
+        Destroy(enemy.gameObject);
+
+        //The projectile is destroyed
+        Destroy(gameObject);
+
+    }
+
+}
+
